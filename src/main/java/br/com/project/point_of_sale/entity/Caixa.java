bcf0eb47529578dfb8bc1829a9e3d7d3bae18dfa
biancadearaujo <bianca8350@gmail.com>
2023-01-04 20:42:45 -0300

    entidade caixa

--- src/main/java/br/com/project/point_of_sale/entity/Caixa.java
+++ src/main/java/br/com/project/point_of_sale/entity/Caixa.java
@@ -1,7 +1,54 @@
 package br.com.project.point_of_sale.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo
+(generator = ObjectIdGenerators.PropertyGenerator.class, property= "idCaixa")
+
+@Entity
+@Table(name="caixa")
 public class Caixa {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name="id_caixa")
     private Integer idCaixa;
+
+    @Column(name="nome")
     private String nome;
+
+    @Column(name="saldo")
     private double saldo;
+
+    public Integer getIdCaixa() {
+        return idCaixa;
+    }
+
+    public void setIdCaixa(Integer idCaixa) {
+        this.idCaixa = idCaixa;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+   
 }
