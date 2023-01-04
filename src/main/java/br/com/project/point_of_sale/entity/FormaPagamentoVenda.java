e95c6b15ab827ff72504cdcf981bc734a26082df
biancadearaujo <bianca8350@gmail.com>
2023-01-04 20:08:54 -0300

    entidade forma pagamento venda

--- src/main/java/br/com/project/point_of_sale/entity/FormaPagamentoVenda.java
+++ src/main/java/br/com/project/point_of_sale/entity/FormaPagamentoVenda.java
@@ -1,7 +1,45 @@
 package br.com.project.point_of_sale.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerator;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo
+(generator = ObjectIdGenerators.PropertyGenerator.class, property = "idFormaPagamentoVenda")
+
+@Entity
+@Table(name="forma_pagamento_venda")
 public class FormaPagamentoVenda {
-    private Integer idVenda;
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name="id_forma_pagamento_venda")
+    private Integer idFormaPagamentoVenda;
     //idFormaPagamentoVenda
+
+    @Column(name="valor")
     private double valor;
+
+    public Integer getIdFormaPagamentoVenda() {
+        return idFormaPagamentoVenda;
+    }
+
+    public void setIdFormaPagamentoVenda(Integer idFormaPagamentoVenda) {
+        this.idFormaPagamentoVenda = idFormaPagamentoVenda;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+ 
 }
