ac51b6fb9d2760baf332857d56ce8834c9642187
biancadearaujo <bianca8350@gmail.com>
2023-01-04 16:25:07 -0300

    entidade categoria

--- src/main/java/br/com/project/point_of_sale/entity/Categoria.java
+++ src/main/java/br/com/project/point_of_sale/entity/Categoria.java
@@ -1,6 +1,26 @@
 package br.com.project.point_of_sale.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo 
+(generator = ObjectIdGenerators.PropertyGenerator.class, property = "idCategoria")
+
+@Entity
+@Table(name="categoria")
 public class Categoria {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name="id_categoria")
     private Integer idCategoria;
+
+    @Column(name="nome")
     private String nome;
 }
