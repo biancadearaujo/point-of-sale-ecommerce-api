82df705293084504527af3fcb7f940e2e517ebc1
biancadearaujo <bianca8350@gmail.com>
2023-01-04 18:46:59 -0300

    entidade item compra

--- src/main/java/br/com/project/point_of_sale/entity/ItemCompra.java
+++ src/main/java/br/com/project/point_of_sale/entity/ItemCompra.java
@@ -1,10 +1,77 @@
 package br.com.project.point_of_sale.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo
+(generator = ObjectIdGenerators.PropertyGenerator.class, property = "idItemCompra")
+
+@Entity
+@Table(name="item_compra")
 public class ItemCompra {
-    private Integer idCompra;
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name="id_item_compra")
+    private Integer idItemCompra;
+
+    @Column(name="numero_item")
     private Integer numeroItem;
     //idProduto
+
+    @Column(name="quantidade")
     private Integer quantidade;
+    
+    @Column(name="valor_unitario")
     private double valorUnitario;
+
+    @Column(name="total_item")
     private double totalItem;
+
+    public Integer getIdItemCompra() {
+        return idItemCompra;
+    }
+
+    public void setIdItemCompra(Integer idItemCompra) {
+        this.idItemCompra = idItemCompra;
+    }
+
+    public Integer getNumeroItem() {
+        return numeroItem;
+    }
+
+    public void setNumeroItem(Integer numeroItem) {
+        this.numeroItem = numeroItem;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(double valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public double getTotalItem() {
+        return totalItem;
+    }
+
+    public void setTotalItem(double totalItem) {
+        this.totalItem = totalItem;
+    }
+
 }
