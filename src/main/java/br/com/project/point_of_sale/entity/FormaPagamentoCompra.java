de4dc14287777c40f94179cdf225193cc6406b80
biancadearaujo <bianca8350@gmail.com>
2023-01-04 20:19:22 -0300

    entidade forma pagamento compra

--- src/main/java/br/com/project/point_of_sale/entity/FormaPagamentoCompra.java
+++ src/main/java/br/com/project/point_of_sale/entity/FormaPagamentoCompra.java
@@ -1,6 +1,38 @@
 package br.com.project.point_of_sale.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo
+(generator = ObjectIdGenerators.PropertyGenerator.class, property = "idFormaPagamentoCompra")
+
+@Entity
+@Table(name="forma_pagamento_compra")
 public class FormaPagamentoCompra {
-    private Integer idFormaPagamento;
-    private String nome;
+    @Column(name="id_forma_pagamento_compra")
+    private Integer idFormaPagamentoCompra;
+
+    @Column(name="valor")
+    private double valor;
+
+    public Integer getIdFormaPagamentoCompra() {
+        return idFormaPagamentoCompra;
+    }
+
+    public void setIdFormaPagamentoCompra(Integer idFormaPagamentoCompra) {
+        this.idFormaPagamentoCompra = idFormaPagamentoCompra;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
 }
