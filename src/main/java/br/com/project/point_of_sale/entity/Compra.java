dfb0deddb4b2abb0f192148997c70abeb2d21f34
biancadearaujo <bianca8350@gmail.com>
2023-01-04 17:54:12 -0300

    entidade compra

--- src/main/java/br/com/project/point_of_sale/entity/Compra.java
+++ src/main/java/br/com/project/point_of_sale/entity/Compra.java
@@ -2,10 +2,67 @@ package br.com.project.point_of_sale.entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo
+(generator = ObjectIdGenerators.PropertyGenerator.class, property = "idCompra")
+
+@Entity
+@Table(name="compra")
 public class Compra {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name="id_compra")
     private Integer idCompra;
+
+    @Column(name="data")
     private LocalDate data;
     //id fornecedor
+
+    @Column(name="total_compra")
     private double totalCompra;
+
+    @Column(name="situacao_compra")
     private String situacaoCompra;
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public double getTotalCompra() {
+        return totalCompra;
+    }
+
+    public void setTotalCompra(double totalCompra) {
+        this.totalCompra = totalCompra;
+    }
+
+    public String getSituacaoCompra() {
+        return situacaoCompra;
+    }
+
+    public void setSituacaoCompra(String situacaoCompra) {
+        this.situacaoCompra = situacaoCompra;
+    }
+
 }
