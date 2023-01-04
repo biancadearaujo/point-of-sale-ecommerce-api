6a5e94eec695cc2ac34a2dda164d7e161243cce1
biancadearaujo <bianca8350@gmail.com>
2023-01-04 17:04:24 -0300

    entidade produto

--- src/main/java/br/com/project/point_of_sale/entity/Produto.java
+++ src/main/java/br/com/project/point_of_sale/entity/Produto.java
@@ -1,12 +1,79 @@
 package br.com.project.point_of_sale.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo
+(generator = ObjectIdGenerators.PropertyGenerator.class, property = "idProduto")
+
+@Entity
+@Table(name="produto")
 public class Produto {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name="id_produto")
     private Integer idProduto;
+
+    @Column(name="nome")
     private String nome;
+
+    @Column(name="quantidadeEstoque")
     private Integer quantidadeEstoque;
+
+    @Column(name="preco")
     private Double preco;
-    private Integer unidade;
+
+    @Column(name="unidade")
+    private String unidade;
+
+    public Integer getIdProduto() {
+        return idProduto;
+    }
 
     //id fornecedor
     //idCategoria
+
+    public void setIdProduto(Integer idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+ 
 }
