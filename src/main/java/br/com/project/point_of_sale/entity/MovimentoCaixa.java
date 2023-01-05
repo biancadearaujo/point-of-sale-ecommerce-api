ceddcc6d175711ae3d4aacae014b860291e2e9ce
biancadearaujo <bianca8350@gmail.com>
2023-01-04 21:04:48 -0300

    entidade movimento caixa

--- src/main/java/br/com/project/point_of_sale/entity/MovimentoCaixa.java
+++ src/main/java/br/com/project/point_of_sale/entity/MovimentoCaixa.java
@@ -2,11 +2,88 @@ package br.com.project.point_of_sale.entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo
+(generator = ObjectIdGenerators.PropertyGenerator.class, property = "idMovimentoCaixa")
+
+@Entity
+@Table(name="movimento_caixa")
 public class MovimentoCaixa {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name="id_movimento_caixa")
     private Integer idMovimentoCaixa;
+
+    @Column(name="numero_movimento")
     private Integer numeroMovimento;
+
+    @Column(name="data_movimento")
     private LocalDate dataMovimento;
+
+    @Column(name="descricao")
     private String descricao;
+
+    @Column(name="tipo_movimento")
     private String tipoMovimento;
+
+    @Column(name="valor")
     private double valor;
+
+    public Integer getIdMovimentoCaixa() {
+        return idMovimentoCaixa;
+    }
+
+    public void setIdMovimentoCaixa(Integer idMovimentoCaixa) {
+        this.idMovimentoCaixa = idMovimentoCaixa;
+    }
+
+    public Integer getNumeroMovimento() {
+        return numeroMovimento;
+    }
+
+    public void setNumeroMovimento(Integer numeroMovimento) {
+        this.numeroMovimento = numeroMovimento;
+    }
+
+    public LocalDate getDataMovimento() {
+        return dataMovimento;
+    }
+
+    public void setDataMovimento(LocalDate dataMovimento) {
+        this.dataMovimento = dataMovimento;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipoMovimento() {
+        return tipoMovimento;
+    }
+
+    public void setTipoMovimento(String tipoMovimento) {
+        this.tipoMovimento = tipoMovimento;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
 }
