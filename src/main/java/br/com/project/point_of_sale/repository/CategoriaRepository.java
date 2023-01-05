71c96d21aad31a6ae6187c2734cdeddd9d6e45f2
biancadearaujo <bianca8350@gmail.com>
2023-01-05 17:16:59 -0300

    Repository Categoria

--- src/main/java/br/com/project/point_of_sale/repository/CategoriaRepository.java
+++ src/main/java/br/com/project/point_of_sale/repository/CategoriaRepository.java
@@ -1,5 +1,11 @@
 package br.com.project.point_of_sale.repository;
 
-public interface CategoriaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.project.point_of_sale.entity.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
     
 }
