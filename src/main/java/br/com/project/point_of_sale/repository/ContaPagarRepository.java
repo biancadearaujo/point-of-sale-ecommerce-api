7aab651cea9e00fbebc5d48fe3473979ef7c655f
biancadearaujo <bianca8350@gmail.com>
2023-01-05 17:17:30 -0300

    Repository Conta Pagar

--- src/main/java/br/com/project/point_of_sale/repository/ContaPagarRepository.java
+++ src/main/java/br/com/project/point_of_sale/repository/ContaPagarRepository.java
@@ -1,5 +1,11 @@
 package br.com.project.point_of_sale.repository;
 
-public interface ContaPagarRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.project.point_of_sale.entity.ContaPagar;
+
+@Repository
+public interface ContaPagarRepository extends JpaRepository<ContaPagar, Integer>{
     
 }
