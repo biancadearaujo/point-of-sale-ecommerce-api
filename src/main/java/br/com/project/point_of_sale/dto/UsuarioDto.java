f1c2cee3ba8cb31a96ef81069cc76c6c49910059
biancadearaujo <bianca8350@gmail.com>
2023-01-13 20:17:19 -0300

    add usuarioDto

--- src/main/java/br/com/project/point_of_sale/dto/UsuarioDto.java
+++ src/main/java/br/com/project/point_of_sale/dto/UsuarioDto.java
@@ -1,5 +1,34 @@
 package br.com.project.point_of_sale.dto;
 
 public class UsuarioDto {
-    
+    private String nome;
+    private String cpfCnpj;
+    private String telefone;
+    private String email;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+   
 }
