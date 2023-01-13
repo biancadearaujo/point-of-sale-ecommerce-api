c83d6ed65a067382f66ad814217f73ad34b31931
biancadearaujo <bianca8350@gmail.com>
2023-01-13 20:17:32 -0300

    add produtoDto

--- src/main/java/br/com/project/point_of_sale/dto/ProdutoDto.java
+++ src/main/java/br/com/project/point_of_sale/dto/ProdutoDto.java
@@ -1,5 +1,34 @@
 package br.com.project.point_of_sale.dto;
 
 public class ProdutoDto {
+    private String nome;
+    private Integer quantidadeEstoque;
+    private Double preco;
+    private String unidade;
     
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Integer getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+    public void setQuantidadeEstoque(Integer quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+    public Double getPreco() {
+        return preco;
+    }
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+    public String getUnidade() {
+        return unidade;
+    }
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+   
 }
