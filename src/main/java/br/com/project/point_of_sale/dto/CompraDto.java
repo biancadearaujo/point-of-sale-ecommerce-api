2f39c89503e4630463fa29b40aca060e76fe256b
biancadearaujo <bianca8350@gmail.com>
2023-01-13 20:14:38 -0300

    add compraDto

--- src/main/java/br/com/project/point_of_sale/dto/CompraDto.java
+++ src/main/java/br/com/project/point_of_sale/dto/CompraDto.java
@@ -1,5 +1,29 @@
 package br.com.project.point_of_sale.dto;
 
+import java.time.LocalDate;
+
 public class CompraDto {
+    private LocalDate data;
+    private double totalCompra;
+    private String situacaoCompra;
     
+    public LocalDate getData() {
+        return data;
+    }
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+    public double getTotalCompra() {
+        return totalCompra;
+    }
+    public void setTotalCompra(double totalCompra) {
+        this.totalCompra = totalCompra;
+    }
+    public String getSituacaoCompra() {
+        return situacaoCompra;
+    }
+    public void setSituacaoCompra(String situacaoCompra) {
+        this.situacaoCompra = situacaoCompra;
+    }
+   
 }
