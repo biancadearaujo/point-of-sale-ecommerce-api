1ce4f5ce45eeee9f14093021ae52ab9ced0fadb4
biancadearaujo <bianca8350@gmail.com>
2023-01-13 20:11:08 -0300

    Caixa DTO

--- src/main/java/br/com/project/point_of_sale/dto/CaixaDto.java
+++ src/main/java/br/com/project/point_of_sale/dto/CaixaDto.java
@@ -1,5 +1,21 @@
 package br.com.project.point_of_sale.dto;
 
 public class CaixaDto {
+    private String nome;
+    private double saldo;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
     
 }
