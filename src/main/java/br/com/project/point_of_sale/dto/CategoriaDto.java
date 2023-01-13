583fc81b4984a0b1d1b2a594566b946ad797b824
biancadearaujo <bianca8350@gmail.com>
2023-01-13 20:11:55 -0300

    add categoria dto

--- src/main/java/br/com/project/point_of_sale/dto/CategoriaDto.java
+++ src/main/java/br/com/project/point_of_sale/dto/CategoriaDto.java
@@ -1,5 +1,14 @@
 package br.com.project.point_of_sale.dto;
 
 public class CategoriaDto {
-    
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
 }
