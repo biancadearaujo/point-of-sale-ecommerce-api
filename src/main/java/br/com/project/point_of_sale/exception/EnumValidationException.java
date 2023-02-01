b253de5ad031a79af0d88b361875f758cdebb096
biancadearaujo <bianca8350@gmail.com>
2023-02-01 16:47:52 -0300

    validador de enum

---
+++ src/main/java/br/com/project/point_of_sale/exception/EnumValidationException.java
@@ -0,0 +1,7 @@
+package br.com.project.point_of_sale.exception;
+
+public class EnumValidationException extends Exception{
+    public EnumValidationException(String message){
+        super(message);
+    }
+}
