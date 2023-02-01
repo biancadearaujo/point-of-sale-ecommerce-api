92dd6bdb411fe17efb41babf99c18b24ebc1c1a0
biancadearaujo <bianca8350@gmail.com>
2023-02-01 16:44:47 -0300

    validacao de usuario

---
+++ src/main/java/br/com/project/point_of_sale/exception/UsuarioException.java
@@ -0,0 +1,7 @@
+package br.com.project.point_of_sale.exception;
+
+public class UsuarioException extends RuntimeException{
+    public UsuarioException(String message){
+        super(message);
+    }
+}
