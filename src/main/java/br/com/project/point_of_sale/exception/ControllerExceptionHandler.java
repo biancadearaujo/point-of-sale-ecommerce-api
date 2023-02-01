be2ad9e1af7e21a7abec20b7100df2161a2fdc90
biancadearaujo <bianca8350@gmail.com>
2023-02-01 16:44:17 -0300

    validacao

--- src/main/java/br/com/project/point_of_sale/exception/ControllerExceptionHandler.java
+++ src/main/java/br/com/project/point_of_sale/exception/ControllerExceptionHandler.java
@@ -1,5 +1,33 @@
 package br.com.project.point_of_sale.exception;
 
-public class ControllerExceptionHandler {
-    
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler extends ResponseEntityExceptionHandler{
+
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+    HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        List<String> erros = new ArrayList<>();
+        for(FieldError error: ex.getBindingResult().getFieldErrors()){
+            erros.add(error.getField() + ": " + error.getDefaultMessage());
+    }
+
+        ErroResposta erroResposta = new ErroResposta(status.value(),
+        "Existem Campos Inválidos, Confira o preechimento", LocalDateTime.now(), erros);
+
+        return super.handleExceptionInternal(ex, erroResposta, headers, status, request);   
+    }
+
 }
