0cdd2bf3cfc375f9289ef57f25295c82f12f2c1c
biancadearaujo <bianca8350@gmail.com>
2023-02-01 16:46:01 -0300

    atualizacoes

--- src/main/java/br/com/project/point_of_sale/controller/EnderecoController.java
+++ src/main/java/br/com/project/point_of_sale/controller/EnderecoController.java
@@ -1,9 +1,14 @@
 package br.com.project.point_of_sale.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.project.point_of_sale.entity.Endereco;
 import br.com.project.point_of_sale.repository.EnderecoRepository;
 
 @RestController
@@ -11,4 +16,10 @@ import br.com.project.point_of_sale.repository.EnderecoRepository;
 public class EnderecoController {
     @Autowired
     private EnderecoRepository enderecoRepository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Endereco saveEndereco(@RequestBody Endereco endereco){
+        return enderecoRepository.save(endereco);
+    }
 }
